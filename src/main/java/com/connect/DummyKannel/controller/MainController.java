49cd22b1a7fbe9f06ec0bb0d861b0b6293cc39e2
naveen-kumar-995 <naveen.kumar@pinnacle.in>
2025-03-09 20:40:51 +0530

    adding logback

--- src/main/java/com/connect/DummyKannel/controller/MainController.java
+++ src/main/java/com/connect/DummyKannel/controller/MainController.java
@@ -2,20 +2,27 @@ package com.connect.DummyKannel.controller;
 
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class MainController {
 
-
-
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+    static long counter;
     @GetMapping("/cgi-bin/sendsms")
-    public ResponseEntity<String>  getRequest()
-    {
-        return   ResponseEntity.status(HttpStatus.OK).body("Dummy Route of web");
+    public ResponseEntity getRequest(@RequestParam Map<String, String> allParams) throws Throwable {
+        counter++;
+        log.info("payload "+allParams + "count "+counter);
+
+        return new  ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
