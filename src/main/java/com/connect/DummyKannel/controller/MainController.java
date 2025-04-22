7858b1a64fafd6bc1fd793ab1a61894f5725518f
naveen-kumar-995 <naveen.kumar@pinnacle.in>
2025-04-22 23:37:47 +0530

    Adding post method

--- src/main/java/com/connect/DummyKannel/controller/MainController.java
+++ src/main/java/com/connect/DummyKannel/controller/MainController.java
@@ -6,10 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -18,11 +16,21 @@ public class MainController {
 
     private static final Logger log = LoggerFactory.getLogger(MainController.class);
     static long counter;
+
+
     @GetMapping("/cgi-bin/sendsms")
-    public ResponseEntity getRequest(@RequestParam Map<String, String> allParams) throws Throwable {
+    public ResponseEntity getRequest(@RequestParam Map<String, String> allParams)  {
         counter++;
         log.info("payload "+allParams + "count "+counter);
-
-        return new  ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        return new  ResponseEntity(HttpStatus.OK);
     }
+
+    @PostMapping("/cgi-bin/sendsms/")
+    public ResponseEntity getRequest(@RequestBody String obj) throws Throwable {
+        counter++;
+        log.info("Recieved Payload" + obj);
+        log.info("Counter : "+ counter);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
 }
